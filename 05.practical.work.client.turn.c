09989adffbe62001bffcf8709bcd6e462380e93a
ducvu2212 <ducvu2212@gmail.com>
2017-03-24 19:54:29 +0700

    Lab 05: Data Transfer

---
+++ 05.practical.work.client.turn.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+
+//*************************client part**********************************
+
+int sockfd;
+	struct sockaddr_in saddr;
+	unsigned short port = 8785;
+	if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Error creating socket\n");
+	}
+	//create the socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// get the host info
+	int i;
+    struct hostent *he;
+    struct in_addr **addr_list;
+
+	if ((he = gethostbyname("localhost")) == NULL) {  
+	    printf("Unknown host\n");
+	}
+
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	memcpy((char *) &saddr.sin_addr.s_addr, he->h_addr_list[0], he->h_length);
+	saddr.sin_port = htons(port);
+
+	//connect
+
+	int connect (int sockfd,
+		const struct sockaddr *saddr,
+		socklen_t addrlen);
+
+	if (connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+		printf("Cannot connect\n");
+	}
+
+	else
+		printf("Successfully connected\n");
+	//connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr));
+	
+
+
+	//data transfer
+	while (1) {
+
+		char buffer[100];
+	    printf ("\nSend: ");
+	    scanf("%s", buffer);
+	 	send(sockfd, buffer, strlen(buffer), 0);
+	    recv(sockfd, buffer, sizeof(buffer), 0);
+	    printf("\nReceive: %s", buffer);
+	}
+	close(sockfd);
+	return 0;
+}
---
+++ 05.practical.work.server.turn.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+	
+//*************************server part**********************************
+
+	int sockfd, clen, clientfd;
+	struct sockaddr_in saddr, caddr;
+	unsigned short port = 8785;
+	if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	printf("Error creating socket\n");
+}
+//create the socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	saddr.sin_port = htons(port);
+
+	//bind socket
+	int bind(int sockfd,
+	const struct sockaddr *bind_addr,
+	socklen_t addrlen);
+
+
+	if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+		printf("Error binding\n");
+	}
+
+	//listen for connection requests
+	if (listen(sockfd, 5) < 0) {
+		printf("Error listening\n");
+	}
+
+	listen(sockfd, 10);
+	clen=sizeof(caddr);
+
+	//accept a new socket
+	if ((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) {
+		printf("Error accepting connection\n");
+	}
+
+	//int client = accept(sockfd, (struct sockaddr_in *) &caddr, &clen);
+
+
+
+	//***********************Data Transfer************************
+	while (1) {
+		char buffer[100];
+	    recv(clientfd, buffer, sizeof(buffer), 0);
+	    printf("\nReceive: %s", buffer);
+	    printf("\nSend: ");
+	    scanf("%s", buffer);
+	    send(clientfd, buffer, strlen(buffer), 0);
+	}
+
+	close (sockfd);
+	return 0;
+}
